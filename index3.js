35207d70e94e112392eadbcac0ff5dceebcc8a26
PerfictGrassland <3416890246@qq.com>
2020-04-08 11:36:43 +0800

    提交

--- index3.js
+++ index3.js
@@ -10,10 +10,10 @@ var TEST_TABLE = 'gradeMes';
 var connection = mysql.createConnection({
     //host:'127.0.0.1',
     host: 'localhost',
-    //port:'3306',
+    port: '3306',
     user: 'root',
     password: '123456',
-    // database: 'runoob'
+    //database: 'runoob'
 });
 
 function singleQuery(connection, sqlStr) {
@@ -34,19 +34,26 @@ function singleQuery(connection, sqlStr) {
 // 连接数据库，指定特定数据表
 connection.connect();
 // console.log("链接状态：", connection.connect());
-connection.query("use " + TEST_DATABASE);
+connection.query("use " + TEST_DATABASE, (error, results, fields) => {
+    if (error) {
+        console.log("连接失败", error);
+    } else {
+        console.log("连接成功", results, fields);
+    }
 
-
-function toCheckDatas() {
-    return singleQuery(connection, `select * from ${TEST_TABLE}`).then((res) => {
-        return res;
-    }).catch((err) => {
-        return err;
-    });
-}
-
-toCheckDatas().then((res) => {
-    console.log("查询结果1：", res);
-}).catch((err) => {
-    console.log("查询结果2：", err);
 });
+
+
+// function toCheckDatas() {
+//     return singleQuery(connection, `select * from ${TEST_TABLE}`).then((res) => {
+//         return res;
+//     }).catch((err) => {
+//         return err;
+//     });
+// }
+
+// toCheckDatas().then((res) => {
+//     console.log("查询结果1：", res);
+// }).catch((err) => {
+//     console.log("查询结果2：", err);
+// });
--- index4.js
+++ index4.js
@@ -1,19 +1,17 @@
 var express = require('express');
 var URL = require("url");
 var app = express();
-const hostname = '114.215.139.176';
-// const port = 8088;
 ///数据库操作
 var mysql = require('mysql');
 var TEST_DATABASE = 'runoob';
 var TEST_TABLE = 'grademes';
 var connection = mysql.createConnection({
     //host:'127.0.0.1',
-    host: hostname,
-    //port:'3306',
+    host: '114.215.139.176',
+    port: '3306',
     user: 'root',
     password: '123456',
-    // database: 'runoob'
+    database: 'runoob'
 });
 
 function singleQuery(connection, sqlStr) {
@@ -34,19 +32,27 @@ function singleQuery(connection, sqlStr) {
 // 连接数据库，指定特定数据表
 connection.connect();
 // console.log("链接状态：", connection.connect());
-connection.query("use " + TEST_DATABASE);
+//connection.query("use " + TEST_DATABASE);
+connection.query("use " + TEST_DATABASE, (error, results, fields) => {
+    if (error) {
+        console.log("连接失败", error);
+    } else {
+        console.log("连接成功", results, fields);
+    }
 
-
-function toCheckDatas() {
-    return singleQuery(connection, `select * from ${TEST_TABLE}`).then((res) => {
-        return res;
-    }).catch((err) => {
-        return err;
-    });
-}
-
-toCheckDatas().then((res) => {
-    console.log("查询结果1：", res);
-}).catch((err) => {
-    console.log("查询结果2：", err);
 });
+
+
+// function toCheckDatas() {
+//     return singleQuery(connection, `select * from ${TEST_TABLE}`).then((res) => {
+//         return res;
+//     }).catch((err) => {
+//         return err;
+//     });
+// }
+
+// toCheckDatas().then((res) => {
+//     console.log("查询结果1：", res);
+// }).catch((err) => {
+//     console.log("查询结果2：", err);
+// });
